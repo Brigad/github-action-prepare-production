171f2cb6e380bd047894b4ff435621de3231ea67
Adrien HARNAY <adrien@harnay.me>
2024-01-04 13:09:26 +0100

    always update title when re-running

--- src/main.ts
+++ src/main.ts
@@ -16,19 +16,13 @@ async function run() {
     const octokit = github.getOctokit(GITHUB_TOKEN!);
 
     const commits = await getPRCommits(octokit, github.context.issue);
-    const pull = await octokit.rest.pulls.get({
-      pull_number: github.context.issue.number,
-      repo: github.context.issue.repo,
-      owner: github.context.issue.owner,
-    });
+
     await octokit.rest.pulls.update({
       pull_number: github.context.issue.number,
       repo: github.context.issue.repo,
       owner: github.context.issue.owner,
       body: commits,
-      ...(pull.data.title.includes('deploy(production)')
-        ? {}
-        : { title: `deploy(production): ${moment().format(`YYYY-MM-DD HH:mm`)}` }),
+      title: `deploy(production): ${moment().format(`YYYY-MM-DD HH:mm`)}`,
     });
   } catch (err) {
     // setFailed logs the message and sets a failing exit code
